81512ea978db27b641157c3c521c87f138cc05cc
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-01-21 13:51:38 +0200

    10.3

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -14,19 +14,6 @@ function clearFilter() {
     $.get(ajaxUrl, updateTableByData);
 }
 
-// http://api.jquery.com/jQuery.ajax/#using-converters
-$.ajaxSetup({
-    converters: {
-        "text json": function (stringData) {
-            var json = JSON.parse(stringData);
-            $(json).each(function () {
-                this.dateTime = this.dateTime.replace('T', ' ').substr(0, 16);
-            });
-            return json;
-        }
-    }
-});
-
 $(function () {
     datatableApi = $("#datatable").DataTable({
         "ajax": {
@@ -37,7 +24,13 @@ $(function () {
         "info": true,
         "columns": [
             {
-                "data": "dateTime"
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type === 'display') {
+                        return formatDate(date);
+                    }
+                    return date;
+                }
             },
             {
                 "data": "description"
