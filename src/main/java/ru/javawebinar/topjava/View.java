5a9adb202cfda8a85fd5bb48be902a8ad24175e5
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2018-01-25 00:49:10 +0200

    11 01 HW10 fix encoding

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,10 +1,11 @@
 package ru.javawebinar.topjava;
 
-public class View {
-    public interface JsonREST {}
-    public interface JsonUI {}
+import javax.validation.groups.Default;
 
-    // https://narmo7.wordpress.com/2014/04/26/how-to-set-up-validation-group-in-springmvc/
-    // http://forum.spring.io/forum/spring-projects/web/117289-validated-s-given-groups-should-consider-default-group-or-not
-    public interface ValidatedUI {}
+public class View {
+    // Validate only form UI/REST
+    public interface Web extends Default {}
+
+    // Validate only when DB save/update
+    public interface Persist extends Default {}
 }
